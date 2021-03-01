32e25b6811bf5bcede9c0960f82cfc48c8c0681b
Kyoso <timo.zaaijer@icloud.com>
2021-03-01 19:05:51 +0100

    Update Chip.js

--- main/src/Chip.js
+++ main/src/Chip.js
@@ -1,1 +1,23 @@
-const { Base } = require('eris-sharder')const { CommandHandler, EventHandler, MongoHandler, RedisHandler } = require('./handlers')const { embedColor, prefix } = require('../config/settings.js')class Chip extends Base {	constructor (client) {		super(client)				this.bot.location = process.cwd()		this.bot.color = embedColor		this.bot.mongo = new MongoHandler()		this.bot.redis = new RedisHandler(this.bot)				new CommandHandler(this.bot)		new EventHandler(this.bot)	}		async launch () {		this.bot.editStatus('online', { name: `${prefix}help`, type: 2 })	}}module.exports = Chip
+const { Base } = require('eris-sharder')
+const { CommandHandler, EventHandler, MongoHandler, RedisHandler } = require('./handlers')
+const { embedColor, prefix } = require('../config/settings.js')
+
+class Chip extends Base {
+	constructor (client) {
+		super(client)
+		
+		this.bot.location = process.cwd()
+		this.bot.color = embedColor
+		this.bot.mongo = new MongoHandler()
+		this.bot.redis = new RedisHandler(this.bot)
+		
+		new CommandHandler(this.bot)
+		new EventHandler(this.bot)
+	}
+	
+	async launch () {
+		this.bot.editStatus('online', { name: `${prefix}help`, type: 2 })
+	}
+}
+
+module.exports = Chip
