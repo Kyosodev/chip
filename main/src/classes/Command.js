6ef31a2317fe90169725f3f406b049e3a54eedf9
Kyoso <timo.zaaijer@icloud.com>
2021-03-01 18:33:50 +0100

    Create Command.js

---
+++ main/src/classes/Command.js
@@ -0,0 +1,19 @@
+class Command {
+	constructor (bot) {
+		this.bot = bot
+		
+		if (this.constructor === Command) throw new TypeError('Abstract class "Command" cannot be instantiated directly')
+		if (this.name === undefined) throw new TypeError('Classes extending "Command" must have a getter "name"')
+		if (this.aliases === undefined) throw new TypeError('Classes extending "Command" must have a getter "aliases"')
+		if (this.run !== undefined) {
+			if (this.run.constructor.name !== 'AsyncFunction')
+				throw new TypeError('Classes extending "Command" must implement "run" as async function')
+		} else throw new TypeError('Classes extending "Command" must implement an async function "run"')
+	}
+	
+	get permissions () {
+		return null
+	}
+}
+
+module.exports = Command
