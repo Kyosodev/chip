c2d75a84a2a78d64e34fe1a6ecbc8936025d155e
Kyoso <timo.zaaijer@icloud.com>
2021-03-01 18:33:37 +0100

    Create CommandHandler.js

---
+++ main/src/handlers/CommandHandler.js
@@ -0,0 +1,32 @@
+const { readdirSync } = require('fs')
+const EventEmitter = require('events')
+
+class CommandHandler extends EventEmitter {
+	constructor (bot) {
+		super()
+		this.bot = bot
+		this.commands = new Map()
+		
+		this.built = false
+		this.build()
+	}
+	
+	build () {
+		if (this.built) return this
+		
+		const commands = readdirSync(this.bot.location + '/src/commands')
+		for (let command of commands) {
+			command = new (require(`../commands/${command}`))(this.bot)
+			command.aliases.forEach((alias) => {
+				this.commands.set(alias, command)
+			})
+			this.commands.set(command.name, command)
+		}
+		
+		this.bot.commands = this.commands
+		this.built = true
+		return this
+	}
+}
+
+module.exports = CommandHandler
